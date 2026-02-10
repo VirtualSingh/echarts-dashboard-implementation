0d78aa22562636af7916f9df257e893397831794
Pushpendra Singh <singh16195@gmail.com>
2026-02-10 11:40:01 +0530

    [PS-10FEB26]: initial bar chart implementation and error resolution

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # echarts-dashboard-implementation
+⚠️ Issue: TS1203 – Export assignment cannot be used when targeting ECMAScript modules
 
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/VirtualSingh/echarts-dashboard-implementation)
+❌ Error Message
+
+```bash
+TS1203: Export assignment cannot be used when targeting ECMAScript modules.
+node_modules/echarts/index.d.ts: export = echarts;
+
+```
+
+🔍 What Caused This?
+
+Angular 21 uses native ECMAScript Modules (ESM) with strict module resolution (moduleResolution: "bundler").
+
+However, the ECharts type definitions still contain this legacy CommonJS syntax:
+```ts
+export = echarts;
+
+```
+This syntax is not allowed when TypeScript is targeting ESM, so the Angular/Vite compiler throws TS1203.
+
+✅ How It Was Resolved
+
+We instructed TypeScript to skip type-checking .d.ts files inside node_modules by adding this to tsconfig.json:
+```json
+{
+  "compilerOptions": {
+    "skipLibCheck": true
+  }
+}
+
+```
--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgxEchartsDirective } from 'ngx-echarts';
 import { CommonModule } from '@angular/common';
+import type { EChartsOption } from 'echarts';
+
 @Component({
   selector: 'app-dashboard',
   imports: [NgxEchartsDirective,CommonModule],
@@ -8,5 +10,62 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
-chartOption:any;
+chartOption:EChartsOption = {
+  title: {
+    text: 'Monthly Sales (Stacked Bar)',
+    left: 'center'
+  },
+
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+
+  legend: {
+    top: 30
+  },
+
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+
+  xAxis: {
+    type: 'category',
+    data: ['Jan', 'Feb', 'Mar', 'Apr', 'May']
+  },
+
+  yAxis: {
+    type: 'value'
+  },
+
+  series: [
+    {
+      name: 'Electronics',
+      type: 'bar',
+      stack: 'total',
+      emphasis: { focus: 'series' },
+      data: [120, 200, 150, 80, 70]
+    },
+    {
+      name: 'Clothing',
+      type: 'bar',
+      stack: 'total',
+      emphasis: { focus: 'series' },
+      data: [60, 100, 90, 140, 130]
+    },
+    {
+      name: 'Grocery',
+      type: 'bar',
+      stack: 'total',
+      emphasis: { focus: 'series' },
+      data: [180, 220, 160, 190, 210]
+    }
+  ]
+};
+
 }
